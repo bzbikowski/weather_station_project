8e9a05c5524c90fe8bdbf4e8e4d1217f78bd0c32
bzbikowski <zbiku112@op.pl>
2019-02-14 14:47:35 +0100

    more comments, reducing size of files

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var model = require("../models/bme_sensor.model");
-var moment = require("moment");
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: 'Weather station'});
 });
+
 
 module.exports = router;
